9c7a5fc3039cbb9b22029ca3b90f778173a65821
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-08 13:33:35 +0600

    Attribute binding

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    description: "This is my product"
+    description: "This is socks",
+    image: "./assets/shocks_green.jpg",
+    url: "https://google.com/search?q=socks"
   }
 });
