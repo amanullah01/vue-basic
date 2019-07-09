e11646d2badb1c255d06ad69219fe6671d402230
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-09 11:59:07 +0600

    List rendering

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ var app = new Vue({
     url: "https://google.com/search?q=socks",
     inStock: true,
     inventory: 100,
-    onSale: true
+    onSale: false,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    varients: [
+      {
+        id: 123,
+        varientColor: "green"
+      },
+      {
+        id: 124,
+        varientColor: "blue"
+      }
+    ]
   }
 });
